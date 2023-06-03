8da63cf99b429264ff6ca6f241af4a20965cee77
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-06-03 17:19:05 +0700

    feat: Set height into 100%

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled'
 export const Block = styled.div`
   --padding-horizontal: 32px;
 
+  height: 100%;
   padding: 60px var(--padding-horizontal);
 
   @media (max-width: 560px) {
