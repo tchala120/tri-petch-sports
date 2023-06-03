642e86587661f7023ad73471d79885df02eb7de8
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-06-03 16:56:07 +0700

    feat: Added constants of images

---
+++ src/constants/images.ts
@@ -0,0 +1,22 @@
+import basketballDesktop from 'assets/basketballer/desktop.svg'
+import basketballTablet from 'assets/basketballer/tablet.svg'
+import basketballMobile from 'assets/basketballer/mobile.svg'
+
+import footerballerDesktop from 'assets/footballer/desktop.svg'
+import footerballerTablet from 'assets/footballer/tablet.svg'
+import footerballerMobile from 'assets/footballer/mobile.svg'
+
+const images = {
+  basketball: {
+    desktop: basketballDesktop,
+    tablet: basketballTablet,
+    mobile: basketballMobile,
+  },
+  footballer: {
+    desktop: footerballerDesktop,
+    tablet: footerballerTablet,
+    mobile: footerballerMobile,
+  },
+}
+
+export default images
